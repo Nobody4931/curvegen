950aaa38af4ae586260a1440a3c584080403d10b
Nobody4931 <ardenxie1@gmail.com>
2023-05-04 19:51:05 -0400

    feat: create simple polynomial class

---
+++ poly.go
@@ -0,0 +1,52 @@
+package main
+
+type Polynomial struct {
+	Degree int
+	Coefficients []float64
+}
+
+func NewPolynomial(degree int) *Polynomial {
+	return &Polynomial{
+		Degree: degree,
+		Coefficients: make([]float64, degree + 1),
+	}
+}
+
+
+func (poly *Polynomial) Coefficient(coIdx int) float64 {
+	if 0 > coIdx || coIdx > poly.Degree {
+		return 0
+	}
+	return poly.Coefficients[coIdx]
+}
+
+
+func (poly *Polynomial) Negate() *Polynomial {
+	result := NewPolynomial(poly.Degree)
+	for coIdx := range result.Coefficients {
+		result.Coefficients[coIdx] = -poly.Coefficient(coIdx)
+	}
+	return result
+}
+
+func (poly *Polynomial) Add(other *Polynomial) *Polynomial {
+	result := NewPolynomial(max(poly.Degree, other.Degree))
+	for coIdx := range result.Coefficients {
+		result.Coefficients[coIdx] = poly.Coefficient(coIdx) + other.Coefficient(coIdx)
+	}
+	return result
+}
+
+func (poly *Polynomial) Sub(other *Polynomial) *Polynomial {
+	return poly.Add(other.Negate())
+}
+
+func (poly *Polynomial) Mul(other *Polynomial) *Polynomial {
+	result := NewPolynomial(poly.Degree + other.Degree)
+	for coIdxPoly, coeffPoly := range poly.Coefficients {
+		for coIdxOther, coeffOther := range other.Coefficients {
+			result.Coefficients[coIdxPoly + coIdxOther] += coeffPoly * coeffOther
+		}
+	}
+	return result
+}
